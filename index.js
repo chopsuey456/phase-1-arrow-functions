6ac475c07c8b3066a6dbae4db86dc3215f7b9b3d
Sara Withrow <saralouise1107@gmail.com>
2023-04-06 19:53:31 -0400

    new

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (num1, num2) => {
+    return (num1 / num2)  
+}
+
+const square = (num1) => {
+    return Math.pow(num1, 2) 
+    
+}
+
+const add = (num1, num2) => {
+    return (num1 + num2)
+}
